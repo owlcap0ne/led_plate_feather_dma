9742fecaab36f3a34eac9ace8cbc7abe8a14bc73
owl <j.h4nsen@gmail.com>
2020-11-11 21:19:28 +0100

    moved timings to struct

--- led_plate_nano-shitty.ino
+++ led_plate_nano-shitty.ino
@@ -26,15 +26,39 @@ typedef struct {
   };
 } LED;
 
+typedef struct {
+  uint8_t   brightness;
+  uint16_t  tOn_R,
+            tOff_R,
+            tOn_G,
+            tOff_G,
+            tOn_B,
+            tOff_B,
+            tOn_I,
+            tOff_I;
+} CONFIG;
+
 //LED led = {0x0A, 0x0A, 0x00};
 #define N_LEDS 24
 LED led[N_LEDS];
+CONFIG config = {
+  .brightness = 31,
+  .tOn_R   = 1,
+  .tOff_R  = 0,
+  .tOn_G   = 0,
+  .tOff_G  = 0,
+  .tOn_B   = 0,
+  .tOff_B  = 0,
+  .tOn_I   = 1,
+  .tOff_I  = 0
+};
 
 const byte numChars = 32;
 char receivedChars[numChars];
 char tmpChars[numChars];
 boolean newData = false;
 
+/* 
 uint8_t brightness = 31;
 uint16_t tOn_R = 1;
 uint16_t tOff_R = 0;
@@ -44,9 +68,10 @@ uint16_t tOn_B = 0;
 uint16_t tOff_B = 0;
 uint16_t tOn_I = 1;
 uint16_t tOff_I = 0;
+*/
 unsigned long t, t_R, t_G, t_B, t_I;
 
-//uint8_t red, green, blue;
+uint8_t red, green, blue;
 
 void APA102(LED *ledarray, uint8_t leds, uint8_t brightness, uint8_t mask) {
   uint8_t *_ledarray = (uint8_t*) ledarray;
@@ -117,31 +142,34 @@ void parseData() {
     //brightness
     Serial.println("Got bright data!");
     strtokIndx = strtok(NULL, ",:;");
-    brightness = (uint8_t) atoi(strtokIndx);
-    if (brightness > 31)
-      brightness = 31;
-    Serial.println(brightness);
+    config.brightness = (uint8_t) atoi(strtokIndx);
+    if (config.brightness > 31)
+      config.brightness = 31;
+    Serial.println(config.brightness);
   }
   else if(strtokIndx[0] == 'c') {
     //color
     Serial.println("Got colorful data!");
     strtokIndx = strtok(NULL, ",:;");
     Serial.println(strtokIndx);
+    red = (uint8_t) atoi(strtokIndx);
     for(int i = 0; i < N_LEDS; i++)
     {
-      led[i].red = (uint8_t) atoi(strtokIndx);
+      led[i].red = red;
     }
     strtokIndx = strtok(NULL, ",:;");
     Serial.println(strtokIndx);
+    green = (uint8_t) atoi(strtokIndx);
     for(int i = 0; i < N_LEDS; i++)
     {
-      led[i].green = (uint8_t) atoi(strtokIndx);
+      led[i].green = green;
     }
     strtokIndx = strtok(NULL, ",:;");
     Serial.println(strtokIndx);
+    blue = (uint8_t) atoi(strtokIndx);
     for(int i = 0; i < N_LEDS; i++)
     {
-      led[i].blue = (uint8_t) atoi(strtokIndx);
+      led[i].blue = blue;
     }
   }
   else if(strtokIndx[0] == 'i') {
@@ -149,9 +177,9 @@ void parseData() {
     Serial.println("Got intermittent data!");
     strtokIndx = strtok(NULL, ",:;");
     Serial.println(strtokIndx);
-    tOn_I = (uint16_t) atoi(strtokIndx) * 1000;
+    config.tOn_I = (uint16_t) atoi(strtokIndx) * 1000;
     strtokIndx = strtok(NULL, ",:;");
-    tOff_I = (uint16_t) atoi(strtokIndx) * 1000;
+    config.tOff_I = (uint16_t) atoi(strtokIndx) * 1000;
   }
   else if(strtokIndx[0] == 'd') {
     if(strtokIndx[1] == 'r') {
@@ -159,37 +187,37 @@ void parseData() {
       Serial.println("Got enduring red data!");
       strtokIndx = strtok(NULL, ",:;");
       Serial.println(strtokIndx);
-      tOn_R = (uint16_t) atoi(strtokIndx);
+      config.tOn_R = (uint16_t) atoi(strtokIndx);
       strtokIndx = strtok(NULL, ",:;");
       Serial.println(strtokIndx);
-      tOff_R = atoi(strtokIndx);
+      config.tOff_R = atoi(strtokIndx);
     }else if(strtokIndx[1] == 'g') {
       //green duration
       Serial.println("Got enduring green data!");
       strtokIndx = strtok(NULL, ",:;");
       Serial.println(strtokIndx);
-      tOn_G = (uint16_t) atoi(strtokIndx);
+      config.tOn_G = (uint16_t) atoi(strtokIndx);
       strtokIndx = strtok(NULL, ",:;");
       Serial.println(strtokIndx);
-      tOff_G = atoi(strtokIndx);
+      config.tOff_G = atoi(strtokIndx);
     }else if(strtokIndx[1] == 'b') {
       //blue duration
       Serial.println("Got enduring blue data!");
       strtokIndx = strtok(NULL, ",:;");
       Serial.println(strtokIndx);
-      tOn_B = (uint16_t) atoi(strtokIndx);
+      config.tOn_B = (uint16_t) atoi(strtokIndx);
       strtokIndx = strtok(NULL, ",:;");
       Serial.println(strtokIndx);
-      tOff_B = atoi(strtokIndx);
+      config.tOff_B = atoi(strtokIndx);
     }else{
       //duration
       Serial.println("Got enduring data!");
       strtokIndx = strtok(NULL, ",:;");
       Serial.println(strtokIndx);
-      tOn_R, tOn_G, tOn_B = (uint16_t) atoi(strtokIndx);
+      config.tOn_R, config.tOn_G, config.tOn_B = (uint16_t) atoi(strtokIndx);
       strtokIndx = strtok(NULL, ",:;");
       Serial.println(strtokIndx);
-      tOff_R, tOff_G, tOff_B = (uint16_t) atoi(strtokIndx);
+      config.tOff_R, config.tOff_G, config.tOff_B = (uint16_t) atoi(strtokIndx);
     }
   }
   else if(strtokIndx[0] == 'h') {
@@ -212,46 +240,46 @@ void loop() {
     parseData();
   
   t = millis();
-  if(t - t_R >= tOn_R)
+  if(t - t_R >= config.tOn_R)
   {
     mask |= MASK_R;
   }
-  if(t - t_R >= tOn_R + tOff_R)
+  if(t - t_R >= config.tOn_R + config.tOff_R)
   {
     mask &= ~MASK_R;
     t_R = millis();
   }
   
-  if(t - t_G >= tOn_G)
+  if(t - t_G >= config.tOn_G)
   {
     mask |= MASK_G;
   }
-  if(t - t_G >= tOn_G + tOff_G)
+  if(t - t_G >= config.tOn_G + config.tOff_G)
   {
     mask &= ~MASK_G;
     t_G = millis();
   }
   
-  if(t - t_B >= tOn_B)
+  if(t - t_B >= config.tOn_B)
   {
     mask |= MASK_B;
   }
-  if(t - t_B >= tOn_B + tOff_B)
+  if(t - t_B >= config.tOn_B + config.tOff_B)
   {
     mask &= ~MASK_B;
     t_B = millis();
   }
 
-  if(t - t_I >= tOn_I)
+  if(t - t_I >= config.tOn_I)
   {
     mask |= MASK_A;
   }
-  if(t - t_I >= tOn_I + tOff_I)
+  if(t - t_I >= config.tOn_I + config.tOff_I)
   {
     mask &= ~MASK_A;
     t_I = millis();
   }
   
-  APA102(led, 24, brightness, mask);
+  APA102(led, 24, config.brightness, mask);
   
 }
